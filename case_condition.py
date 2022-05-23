59671e54706c006d35ba56ab4d657268e5d1d4e3
Wendi Barmara <wendibarmara@gmail.com>
2022-05-23 21:33:48 +0700

    case condition

---
+++ case_condition.py
@@ -0,0 +1,28 @@
+def printMonth(num):
+    if num == 1:
+        month =' January'
+    elif num == 2:
+        month = 'February'
+    elif num == 3:
+        month = 'March'
+    elif num == 4:
+        month= 'April'
+    elif num == 5:
+        month= 'May'
+    elif num == 6:
+        month = 'June'
+    elif num == 7:
+        month = 'July'
+    elif num == 8:
+        month = 'August'
+    elif num == 9:
+        month= 'September'
+    elif num == 10:
+        month= 'October'
+    elif num == 11:
+        month= 'November'
+    elif num == 12:
+        month= 'December'
+    return month
+
+print('Month is:', printMonth(10))
