e5fd9e557d9cfd050aa84c9c88fe5cd02bc8b816
Wendi Barmara <wendibarmara@gmail.com>
2022-05-23 21:30:39 +0700

    elif

---
+++ elif.py
@@ -0,0 +1,15 @@
+# if-elif statement example
+
+city= 'Bangalore'
+
+if city == 'Delhi':
+    print("city is Delhi")
+
+elif city == "Hyderabad":
+    print("city is Hyderabad")
+
+elif city== "Bangalore":
+    print("city is Bangalore")
+
+else:
+    print("city isn't Bangalore, Delhi or Hyderabad")
